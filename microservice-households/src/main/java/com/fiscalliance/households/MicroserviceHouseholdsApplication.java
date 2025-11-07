d9169513d84e3862f2d16de1e4ea3e6608034246
RubenGenaro10 <rubengenaro10@gmail.com>
2025-11-07 00:53:07 -0500

    fix(proyect): se agrego el microservicio households y eureka

---
+++ microservice-households/src/main/java/com/fiscalliance/households/MicroserviceHouseholdsApplication.java
@@ -0,0 +1,13 @@
+package com.fiscalliance.households;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceHouseholdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceHouseholdsApplication.class, args);
+	}
+
+}
